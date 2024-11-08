18b843bf84fe5b54765067fad450af2ffe69a6bf
Shomari <nyusternie@sdot.io>
2024-11-08 13:50:08 -0500

    Update homepage.

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -34,15 +34,18 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+    const {siteConfig} = useDocusaurusContext()
+
+    return (
+        <Layout
+            title={siteConfig.title}
+            description="Cast Poker for Farcaster."
+        >
+            <HomepageHeader />
+
+            <main>
+                <HomepageFeatures />
+            </main>
+        </Layout>
+    )
 }
