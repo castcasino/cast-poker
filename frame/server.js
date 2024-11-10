b53476a4d748612b79cd893427bd9ccf6aa3f57b
Shomari <nyusternie@sdot.io>
2024-11-10 07:47:29 -0500

    Add Viem.

--- frame/server.js
+++ frame/server.js
@@ -1,57 +1,63 @@
-import express from "express";
+import express from 'express'
 
 // Constants
-const isProduction = process.env.NODE_ENV === "production";
-const port = process.env.PORT || 5173;
-const base = process.env.BASE || "/";
+const isProduction = process.env.NODE_ENV === 'production'
+// const port = process.env.PORT || 5173
+const port = process.env.PORT || 5000
+const base = process.env.BASE || '/'
 
 // Create http server
-const app = express();
+const app = express()
 
 // Add Vite or respective production middlewares
-let vite;
+let vite
+
 if (!isProduction) {
-  const { createServer } = await import("vite");
-  vite = await createServer({
-    server: { middlewareMode: true },
-    appType: "custom",
-    base,
-  });
-  app.use(vite.middlewares);
+    const { createServer } = await import('vite')
+
+    vite = await createServer({
+        server: { middlewareMode: true },
+        appType: 'custom',
+        base,
+    })
+
+    app.use(vite.middlewares)
 } else {
-  const compression = (await import("compression")).default;
-  const sirv = (await import("sirv")).default;
-  app.use(compression());
-  app.use(base, sirv("./dist/client", { extensions: [] }));
+    const compression = (await import('compression')).default
+    const sirv = (await import('sirv')).default
+
+    app.use(compression())
+    app.use(base, sirv('./dist/client', { extensions: [] }))
 }
 
 // Serve HTML
-app.use("*", async (req, res) => {
-  try {
-    const url = req.originalUrl.replace(base, "");
+app.use('*', async (req, res) => {
+    try {
+        const url = req.originalUrl.replace(base, '')
 
-    /**
-     * @type {import('express').Handler}
-     */
-    let handleRequest;
-    if (!isProduction) {
-      // Always read fresh template in development
-      handleRequest = (await vite.ssrLoadModule("/src/entry-server.tsx"))
-        .handleRequest;
-    } else {
-      handleRequest = (await import("./dist/server/entry-server.js"))
-        .handleRequest;
+        /**
+         * @type {import('express').Handler}
+         */
+        let handleRequest;
+
+        if (!isProduction) {
+            // Always read fresh template in development
+            handleRequest = (await vite.ssrLoadModule('/src/entry-server.tsx'))
+                .handleRequest
+        } else {
+            handleRequest = (await import('./dist/server/entry-server.js'))
+                .handleRequest
+        }
+
+        handleRequest(req, res)
+    } catch (e) {
+        vite?.ssrFixStacktrace(e)
+        console.log(e.stack)
+        res.status(500).end(e.stack)
     }
-
-    handleRequest(req, res);
-  } catch (e) {
-    vite?.ssrFixStacktrace(e);
-    console.log(e.stack);
-    res.status(500).end(e.stack);
-  }
-});
+})
 
 // Start http server
 app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
-});
+    console.log(`Server started at http://localhost:${port}`)
+})
