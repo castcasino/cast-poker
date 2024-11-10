6f1c558d81ed6186bbb58736f48853c9ad795b35
Shomari <nyusternie@sdot.io>
2024-11-10 10:23:44 -0500

    Add mini app.

---
+++ app/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 6000
+
+app.get('/', (req, res) => {
+    res.send('Cast Poker Mini App is running...')
+})
+
+app.listen(port, () => {
+  console.log(`Cast Poker Mini App listening on port ${port}`)
+})
