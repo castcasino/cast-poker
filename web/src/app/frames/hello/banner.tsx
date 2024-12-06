2c4b0c6daf0e5839959e72127cab5e252e9d491b
Shomari <nyusternie@sdot.io>
2024-12-06 18:08:52 -0500

    Add frame banner.

--- web/src/app/frames/hello/banner.tsx
+++ web/src/app/frames/hello/banner.tsx
@@ -2,7 +2,7 @@ import { ImageResponse } from "next/og";
 
 export const runtime = "edge";
 
-export const alt = "Hello Frame";
+export const alt = "Hello Banner";
 export const size = {
   width: 600,
   height: 400,
@@ -14,7 +14,7 @@ export default async function Image() {
   return new ImageResponse(
     (
       <div tw="h-full w-full flex flex-col justify-center items-center relative bg-white">
-        <h1 tw="text-6xl">Hello, world!</h1>
+        <h1 tw="text-6xl">Hello, banner!</h1>
       </div>
     ),
     {
