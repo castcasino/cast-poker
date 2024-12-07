8f7cd9618ea3910aec2a410212e1bde440c73109
Shomari <nyusternie@sdot.io>
2024-12-07 03:07:30 -0500

    Add context to header.

--- web/src/components/ui/Header.tsx
+++ web/src/components/ui/Header.tsx
@@ -1,14 +1,45 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import sdk, { type FrameContext } from '@farcaster/frame-sdk'
+
 export function Header({ tableid }) {
+    const [isSDKLoaded, setIsSDKLoaded] = useState(false)
+    const [context, setContext] = useState<FrameContext>()
+
+    useEffect(() => {
+        const load = async () => {
+            setContext(await sdk.context)
+            sdk.actions.ready()
+        }
+
+        if (sdk && !isSDKLoaded) {
+            setIsSDKLoaded(true)
+            load()
+        }
+    }, [ isSDKLoaded ])
+
+    if (!isSDKLoaded) {
+        return <div>Loading. Please wait...</div>
+    }
+
     return (
         <header className="w-full sm:w-[640px] mx-auto h-[100px] z-10 flex justify-between bg-sky-500">
-            <div className="">
+            <div className="w-2/3">
                 <span>
                     {tableid}
                 </span>
 
-                <div>
+                {(context && <p className="text-slate-700">
+                    { JSON.stringify(context) }
+                </p>)}
 
-                </div>
+                {(!context && <p className="text-slate-700 text-xs">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Vero velit at sequi consequuntur.
+                </p>)}
+
             </div>
 
             <section className="m-3 px-5 py-2 flex flex-col items-center rounded-lg border-2 border-lime-500 bg-lime-200">
