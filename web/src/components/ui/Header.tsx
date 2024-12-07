ae6601835c47d3e66d120320328fda7f2d751ca6
Shomari <nyusternie@sdot.io>
2024-12-07 06:27:45 -0500

    Fix context.

--- web/src/components/ui/Header.tsx
+++ web/src/components/ui/Header.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import Image from 'next/image'
 
 import sdk, { type FrameContext } from '@farcaster/frame-sdk'
 
@@ -26,7 +27,29 @@ export function Header({ tableid }: { tableid: string }) {
 
     return (
         <header className="w-full sm:w-[640px] mx-auto h-[100px] z-10 flex justify-between bg-sky-500">
-            <div className="w-2/3">
+            <section className="w-2/3">
+                {(context && <a href="javascript://" className="group block shrink-0">
+                    <div className="flex items-center">
+                        <Image
+                            className="inline-block size-9 rounded-full"
+                            src={context?.user?.pfpUrl || 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'}
+                            alt={context?.user?.displayName || ''}
+                            width={500}
+                            height={500}
+                        />
+
+                        <div className="ml-3">
+                            <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">
+                                {context.user.displayName}
+                            </p>
+
+                            <p className="text-xs font-medium text-gray-500 group-hover:text-gray-700">
+                                {context.user.username}
+                            </p>
+                        </div>
+                    </div>
+                </a>)}
+
                 <span>
                     {tableid}
                 </span>
@@ -40,7 +63,7 @@ export function Header({ tableid }: { tableid: string }) {
                     Vero velit at sequi consequuntur.
                 </p>)}
 
-            </div>
+            </section>
 
             <section className="m-3 px-5 py-2 flex flex-col items-center rounded-lg border-2 border-lime-500 bg-lime-200">
                 <span className="text-2xl font-medium text-lime-600">
