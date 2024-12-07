e048d7ecd9778e275f58137b3fb84fe027461085
Shomari <nyusternie@sdot.io>
2024-12-07 13:10:45 -0500

    Debug image component.

--- web/src/components/ui/Header.tsx
+++ web/src/components/ui/Header.tsx
@@ -40,6 +40,18 @@ export function Header({ tableid }: { tableid: string }) {
                             alt={context?.user?.displayName || ''}
                         />
 
+                        <img
+                            className="inline-block size-9 rounded-full"
+                            src={'https://wsrv.nl/?url=' + context?.user?.pfpUrl || 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'}
+                            alt={context?.user?.displayName || ''}
+                        />
+
+                        <img
+                            className="inline-block size-9 rounded-full"
+                            src={context?.user?.pfpUrl || 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'}
+                            alt={context?.user?.displayName || ''}
+                        />
+
                         <div className="ml-3">
                             <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">
                                 {context.user.displayName}
