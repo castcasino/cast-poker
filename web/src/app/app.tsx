825d0f94de04c328b6ec66dab020f5b54175fb1b
Shomari <nyusternie@sdot.io>
2024-12-07 03:22:15 -0500

    Alt fix.

--- web/src/app/app.tsx
+++ web/src/app/app.tsx
@@ -6,25 +6,27 @@ import { Footer } from '~/components/ui/Footer'
 import { Header } from '~/components/ui/Header'
 import { Navbar } from '~/components/ui/Navbar'
 
-function withProps(WrappedComponent: React.FC) {
-    return function EnhancedComponent({ tableid }: { tableid: string }) {
-        return <WrappedComponent tableid={tableid} />
-    }
-}
+// function withProps(WrappedComponent: React.FC) {
+//     return function EnhancedComponent({ tableid }: { tableid: string }) {
+//         return <WrappedComponent tableid={tableid} />
+//     }
+// }
 
 export default function App(
     { screenid, tableid }: { screenid: string, tableid: string } = { screenid: 'lobby', tableid: '' }
 ) {
     /* Initialize screen manager. */
-    const Screen = withProps(
-        dynamic(() => import('~/screens/' + screenid), { ssr: false })
-    )
+    const Screen = dynamic(() => import('~/screens/' + screenid), { ssr: false })
+
+    function ScreenCaller({ tableid }) {
+        return <Screen tableid={tableid} />
+    }
 
     return <main className="w-screen h-screen flex flex-col justify-between bg-gradient-to-l from-slate-600 to-slate-800">
         <Header tableid={tableid} />
 
         <main className="w-full sm:w-[640px] mx-auto py-4 px-2 bg-gradient-to-r from-slate-50 to-slate-200 flex-1 overflow-y-scroll">
-            <Screen tableid={tableid} />
+            <ScreenCaller tableid={tableid} />
         </main>
 
         <>
