006d6dc7c75bdaf56d071b8c394efee49a20b603
Shomari <nyusternie@sdot.io>
2024-12-11 16:57:27 -0500

    Update manifest header.

--- web/src/app/.well-known/farcaster.json/route.ts
+++ web/src/app/.well-known/farcaster.json/route.ts
@@ -4,7 +4,7 @@ export async function GET() {
     const config = {
         accountAssociation: {
             header:
-                'eyJmaWQiOjg4MjA0MCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDdCZTRDNUMzZkY2M0RDRTMzODJFMjlCMWI4MTNFM2RjNDhhQWNERTEifQ',
+                'eyJmaWQiOjg4MjA0MCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweGE5NjhkZGY0NzY5Y2U2OTFhYTFkM2VhMjE4ZjliMDAzOTNiMmNmNzIifQ==',
             payload: 'eyJkb21haW4iOiJjYXN0LnBva2VyIn0=',
             signature:
                 'MHgwNDA4NmJjMTkxMDZiN2M0NTUzMzZmODliYTY0YzkzMThiYzE2NTMzMjJlZjFlZWVkODc1OWY1MzRjOTEyZmQxMmU4ZTA2MmJhYWJlZGI0MjZkZmFhMWZhMGQyNjZmNDQ2MTFhNzdlMjBiNjc4M2Q3OTM5ZTg5OWE3MjZhMTU2YjFj',
@@ -22,3 +22,6 @@ export async function GET() {
 
     return Response.json(config)
 }
+
+// eyJmaWQiOjg4MjA0MCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDdCZTRDNUMzZkY2M0RDRTMzODJFMjlCMWI4MTNFM2RjNDhhQWNERTEifQ
+// eyJmaWQiOjg4MjA0MCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweGE5NjhkZGY0NzY5Y2U2OTFhYTFkM2VhMjE4ZjliMDAzOTNiMmNmNzIifQ==
