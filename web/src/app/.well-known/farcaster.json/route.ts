ecbadc09c5299a888728e407ad223f141476f76d
Shomari <nyusternie@sdot.io>
2024-12-11 21:54:27 -0500

    Fix manifest.

--- web/src/app/.well-known/farcaster.json/route.ts
+++ web/src/app/.well-known/farcaster.json/route.ts
@@ -5,9 +5,9 @@ export async function GET() {
         accountAssociation: {
             header:
                 'eyJmaWQiOjg4MjA0MCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDdCZTRDNUMzZkY2M0RDRTMzODJFMjlCMWI4MTNFM2RjNDhhQWNERTEifQ',
-            payload: 'eyJkb21haW4iOiJjYXN0LnBva2VyIn0=',
+            payload: 'eyJkb21haW4iOiJjYXN0LnBva2VyIn0',
             signature:
-                'MHgwNDA4NmJjMTkxMDZiN2M0NTUzMzZmODliYTY0YzkzMThiYzE2NTMzMjJlZjFlZWVkODc1OWY1MzRjOTEyZmQxMmU4ZTA2MmJhYWJlZGI0MjZkZmFhMWZhMGQyNjZmNDQ2MTFhNzdlMjBiNjc4M2Q3OTM5ZTg5OWE3MjZhMTU2YjFj',
+                'MHgwNDA4NmJjMTkxMDZiN2M0NTUzMzZmODliYTY0YzkzMThiYzE2NTMzMjJlZjFlZWVkODc1OWY1MzRjOTEyZmQxMmU4ZTA2MmJhYWJlZGI0MjZkZmFhMWZhMGQyNjZmNDQ2MTFhNzdlMjBiNjc4M2Q3OTM5ZTg5OWE3MjZhMTU2YjFj'
         },
         frame: {
             version: '0.0.0',
