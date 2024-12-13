03ea0d9b4150fc55ac4fc03a18f6b2db0cc69412
Shomari <nyusternie@sdot.io>
2024-12-12 23:51:45 -0500

    Enable session manager.

--- web/src/app/app.tsx
+++ web/src/app/app.tsx
@@ -1,5 +1,10 @@
 'use client'
 
+'use client'
+
+import { useEffect, useState } from 'react'
+import sdk, { type FrameContext } from '@farcaster/frame-sdk'
+
 // Import components.
 import { Footer } from '~/components/ui/Footer'
 import { Header } from '~/components/ui/Header'
@@ -22,6 +27,9 @@ import Promote from '~/screens/earn/Promote'
 // Import screens.
 import Lobby from '~/screens/Lobby'
 
+/* Import session manager. */
+import { sessionManager } from '~/lib/sessionManager'
+
 export default function App({
     screenid,
     tableid,
@@ -32,6 +40,26 @@ export default function App({
     screenid: 'lobby',
     tableid: '',
 }) {
+    const [isSDKLoaded, setIsSDKLoaded] = useState(false)
+    const [context, setContext] = useState<FrameContext>()
+
+    useEffect(() => {
+        const load = async () => {
+            setContext(await sdk.context)
+            sdk.actions.ready()
+        }
+
+        if (sdk && !isSDKLoaded) {
+            setIsSDKLoaded(true)
+            load()
+        }
+    }, [ isSDKLoaded ])
+
+    useEffect(() => {
+        console.log('THIS IS THE CONTEXT WE NEED TO STORE', context)
+        sessionManager(JSON.stringify(context))
+    }, [ context ])
+
     return <main className="w-screen h-screen overflow-hidden flex flex-col justify-between bg-gradient-to-l from-slate-600 to-slate-800">
         <Header tableid={tableid} />
 
--- web/src/lib/sessionManager.ts
+++ web/src/lib/sessionManager.ts
@@ -1,28 +1,4 @@
-'use client'
+export const sessionManager = (context: string) => {
 
-import { useEffect, useState, useCallback } from 'react'
-
-import sdk, { type FrameContext } from '@farcaster/frame-sdk'
-
-export const sessionManager = () => {
-    const [isSDKLoaded, setIsSDKLoaded] = useState(false)
-    const [context, setContext] = useState<FrameContext>()
-
-    const castPromo = useCallback(() => {
-        sdk.actions.openUrl('https://warpcast.com/~/compose')
-    }, [])
-
-    useEffect(() => {
-        const load = async () => {
-            setContext(await sdk.context)
-            sdk.actions.ready()
-        }
-
-        if (sdk && !isSDKLoaded) {
-            setIsSDKLoaded(true)
-            load()
-        }
-    }, [ isSDKLoaded ])
-
-    console.log('SESSION MANAGER HAS BEEN INITIALIZED!!')
+    console.log('SESSION MANAGER HAS BEEN INITIALIZED!!', context)
 }
