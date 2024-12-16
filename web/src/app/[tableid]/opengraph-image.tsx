35d863e0280f2612b24aa60d3ee7fdc5b3b9166c
Shomari <nyusternie@sdot.io>
2024-12-15 21:27:53 -0500

    Update (main) frame UI.

--- web/src/app/[tableid]/opengraph-image.tsx
+++ web/src/app/[tableid]/opengraph-image.tsx
@@ -22,14 +22,44 @@ export default async function Image({ params }: Props) {
 
     return new ImageResponse(
         (
-            <div tw="h-full w-full flex flex-col justify-center items-center relative bg-white">
-                <span tw="text-6xl">
-                    Watch the LIVE action @
-                </span>
+            <div tw="w-full h-full flex flex-col justify-center items-center bg-green-50">
+                <section tw="flex items-center">
+                    <img
+                        src="https://assets.cast.casino/poker-table.png"
+                        tw="h-20 w-20"
+                    />
 
-                <span tw="text-6xl">
-                    Table # {tableid}
-                </span>
+                    <span tw="pl-10 text-lime-600 text-5xl font-light tracking-wider italic">
+                        Would you like to play a game at Table # {tableid}?
+                    </span>
+                </section>
+
+                <section tw="mt-10 flex">
+                    <img
+                        src="https://assets.cast.casino/cards_01/AS.svg"
+                        tw="mx-5 h-48 w-36 border-2 border-slate-700"
+                    />
+
+                    <img
+                        src="https://assets.cast.casino/cards_01/KS.svg"
+                        tw="mx-5 h-48 w-36 border-2 border-slate-700"
+                    />
+
+                    <img
+                        src="https://assets.cast.casino/cards_01/QS.svg"
+                        tw="mx-5 h-48 w-36 border-2 border-slate-700"
+                    />
+
+                    <img
+                        src="https://assets.cast.casino/cards_01/JS.svg"
+                        tw="mx-5 h-48 w-36 border-2 border-slate-700"
+                    />
+
+                    <img
+                        src="https://assets.cast.casino/cards_01/TS.svg"
+                        tw="mx-5 h-48 w-36 border-2 border-slate-700"
+                    />
+                </section>
             </div>
         ),
         { ...size }
