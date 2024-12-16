f24ce25e70b15b5a0163462390348a79385c4fe8
Shomari <nyusternie@sdot.io>
2024-12-16 17:30:49 -0500

    Fix typings.

--- web/src/screens/lounge/Tables.tsx
+++ web/src/screens/lounge/Tables.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 import sdk, { type FrameContext } from '@farcaster/frame-sdk'
 
 import axios from 'axios'
-import { formatEther } from 'viem'
+// import { formatEther } from 'viem'
 
 import { truncateAddress } from '~/lib/truncateAddress'
 
