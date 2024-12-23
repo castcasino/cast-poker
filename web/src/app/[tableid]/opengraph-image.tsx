66ddc57fa225cd963672d5b678973fd9f8ee6c3e
Shomari <nyusternie@sdot.io>
2024-12-22 23:28:55 -0500

    Update splash media.

--- web/src/app/[tableid]/opengraph-image.tsx
+++ web/src/app/[tableid]/opengraph-image.tsx
@@ -1,8 +1,6 @@
-import Image from 'next/image'
 import { ImageResponse } from 'next/og'
 
 import axios from 'axios'
-// import numeral from 'numeral'
 
 export const runtime = 'edge'
 
@@ -54,7 +52,7 @@ export default async function OgImage({ params }: Props) {
             (
                 <div tw="w-full h-full flex flex-col justify-center items-center bg-red-800">
                     <section tw="flex items-center">
-                        <Image
+                        <img
                             src="https://assets.cast.casino/poker-table.png"
                             tw="h-20 w-20"
                             alt="Poker table"
@@ -66,31 +64,31 @@ export default async function OgImage({ params }: Props) {
                     </section>
 
                     <section tw="mt-10 flex">
-                        <Image
+                        <img
                             src="https://assets.cast.casino/cards_01/covers/astronaut.svg"
                             tw="mx-1 w-26 border-2 border-slate-700"
                             alt="Unset card of astronaut"
                         />
 
-                        <Image
+                        <img
                             src="https://assets.cast.casino/cards_01/covers/astronaut.svg"
                             tw="mx-1 w-26 border-2 border-slate-700"
                             alt="Unset card of astronaut"
                         />
 
-                        <Image
+                        <img
                             src="https://assets.cast.casino/cards_01/covers/astronaut.svg"
                             tw="mx-1 w-26 border-2 border-slate-700"
                             alt="Unset card of astronaut"
                         />
 
-                        <Image
+                        <img
                             src="https://assets.cast.casino/cards_01/covers/astronaut.svg"
                             tw="mx-1 w-26 border-2 border-slate-700"
                             alt="Unset card of astronaut"
                         />
 
-                        <Image
+                        <img
                             src="https://assets.cast.casino/cards_01/covers/astronaut.svg"
                             tw="mx-1 w-26 border-2 border-slate-700"
                             alt="Unset card of astronaut"
