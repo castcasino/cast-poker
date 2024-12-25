1c596f1c9f4c7585188a2825d4b92053e8662872
Shomari <nyusternie@sdot.io>
2024-12-25 13:49:22 -0500

    Fix link.

--- web/src/screens/earn/Host.tsx
+++ web/src/screens/earn/Host.tsx
@@ -206,7 +206,7 @@ console.log('TRANSACTION SUCCESSFUL', hash)
                 <div className="border-b border-gray-200">
                     <nav className="-mb-px flex" aria-label="Tabs">
                         {/* <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" --> */}
-                        <Link href={`/${tableid}/promote`} className="w-1/2 border-b-2 border-transparent px-1 py-4 text-center text-lg font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">
+                        <Link href={`/${tableid}/share`} className="w-1/2 border-b-2 border-transparent px-1 py-4 text-center text-lg font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">
                             Promotion
                         </Link>
 
