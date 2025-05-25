846948ccd65116fcfb1f3ae99daa7df3ebff85b0
Shomari <nyusternie@sdot.io>
2025-05-25 14:06:50 -0400

    Add SDK test.

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,8 @@
                 Redirecting you to a random table...
             </p>
         </div>
+
+        <pre class="p-10">is this a Mini App? {{isMiniApp}}</pre>
     </main>
 </template>
 
@@ -15,4 +17,25 @@ useHead({
         { name: 'description', content: 'Cast Poker offers a premium blockchain gaming experience for Players over 18+.' }
     ],
 })
+
+import { sdk } from '@farcaster/frame-sdk'
+
+const isMiniApp = ref(null)
+
+const init = async () => {
+    /* Request Mini App flag. */
+    isMiniApp.value = await sdk.isInMiniApp()
+console.log('isMiniApp', isMiniApp.value)
+}
+
+onMounted(() => {
+    console.log('Mounted!')
+
+    init()
+})
+
+// onBeforeUnmount(() => {
+//     console.log('Before Unmount!')
+//     // Now is the time to perform all cleanup operations.
+// })
 </script>
