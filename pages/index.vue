471d81aae9716104ffec73377283bf8bea5cc5cc
Shomari <nyusternie@sdot.io>
2025-05-26 02:44:57 -0400

    Update debugging.

--- pages/index.vue
+++ pages/index.vue
@@ -8,21 +8,15 @@
 
         <pre class="p-10">is this a Mini App? {{isMiniApp}}</pre>
 
-        <button @click="signin" class="p-5 bg-blue-100 border border-blue-300">
-            sign in
-        </button>
+        <pre v-if="user" class="p-10">{{JSON.stringify(user, null, 2)}}</pre>
 
-        <button @click="auth" class="p-5 bg-blue-100 border border-blue-300">
-            quick auth
-        </button>
+        <pre v-if="jwt" class="text-xs">{{jwt}}</pre>
 
         <section class="p-10">
-            <h2>App Context (delayed)</h2>
+            <h2>App Context</h2>
             <pre class="p-5 bg-amber-100 border border-amber-300">{{JSON.stringify(ctx)}}</pre>
             <pre class="p-5 bg-amber-100 border border-amber-300">{{JSON.stringify(ctx?.location, null, 2)}}</pre>
-            <pre class="p-5 bg-amber-100 border border-amber-300">{{JSON.stringify(ctx?.user, null, 2)}}</pre>
             <pre class="p-5 bg-amber-100 border border-amber-300">{{JSON.stringify(ctx?.client, null, 2)}}</pre>
-            <pre class="p-5 bg-amber-100 border border-amber-300">{{JSON.stringify(jwt, null, 2)}}</pre>
         </section>
     </main>
 </template>
@@ -41,32 +35,41 @@ useHead({
 /* Initialize local handlers. */
 const ctx = ref(null)
 const jwt = ref(null)
+const user = ref(null)
+
 const isMiniApp = ref(null)
 
 const init = async () => {
     /* Request Mini App flag. */
     isMiniApp.value = await sdk.isInMiniApp()
-console.log('isMiniApp', isMiniApp.value)
 
-    /* Set app context. */
-ctx.value = await sdk.context
+    /* Validate Mini App. */
+    if (isMiniApp) {
+        /* Request app context. */
+        const context = await sdk.context
+
+// FIXME FOR DEBUGGING ONLY
+ctx.value = context
+
+        /* Assign user. */
+        user.value = context.user
+
+        /* Auto-authorize user. */
+        await auth()
+    }
 }
 
 const auth = async () => {
+    /* Make (quick) authorization request. */
     const { token } = await sdk.experimental.quickAuth()
-console.log('AUTH TOKEN', token)
 
+    // TODO SAVE TO SESSION
+
+// FIXME FOR DEBUGGING ONLY
     jwt.value = token
 }
 
-const signin = async () => {
-    const account = await sdk.actions.signIn()
-console.log('ACCOUNT', account)
-}
-
 onMounted(() => {
-    console.log('Mounted!')
-
     init()
 })
 
